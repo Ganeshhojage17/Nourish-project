14e5e5c4a6263463c0e55da1b6f4941fbac30850
Ganeshhojage17 <ganeshhojage22@gmail.com>
2023-04-16 18:13:16 +0530

    project commit

--- reservation/obbs.sql
+++ reservation/obbs.sql
@@ -43,7 +43,8 @@ CREATE TABLE `tbladmin` (
 --
 
 INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
-(1, 'Admin', 'admin', 5689784589, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2020-01-21 11:48:13');
+(1, 'Admin', 'admin', 5689784589, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2023-01-21 11:48:13'),
+(2, 'Ganesh', 'admin', 828298983, 'ganesh22@gmail.com', 'fa1d87bc7f85769ea9dee2e4957321ae', '2023-03-22 11:50:15');
 
 -- --------------------------------------------------------
 
@@ -96,13 +97,10 @@ CREATE TABLE `tblcontact` (
 --
 
 INSERT INTO `tblcontact` (`ID`, `Name`, `Email`, `Message`, `EnquiryDate`, `IsRead`) VALUES
-(1, 'Kiran', 'kran@gmail.com', 'cost of volvo place pritampura to dwarka', '2021-07-05 07:26:24', 1),
-(2, 'Sarita Pandey', 'sar@gmail.com', 'huiyuihhjjkhkjvhknv iyi tuyvuoiup', '2021-07-09 12:48:40', 1),
-(3, 'Test', 'test@gmail.com', 'Want to know price of forest cake', '2021-07-16 12:51:06', 1),
-(4, 'Anuj', 'ak330@gmail.com', 'This is for testing.', '2021-07-18 14:35:50', 1),
-(5, 'Meenu Kumari', 'meenu@gmail.com', 'gugjhjhgjhgjwerte', '2022-02-15 06:30:58', NULL),
-(6, 'Anuj', 'ak@gmail.com', 'Testing', '2022-02-19 16:04:18', 1),
-(7, 'Anuj', 'akj@gmail.com', 'This is for testing.', '2022-02-19 17:53:03', 1);
+(1, 'Aditya', 'adi@gmail.com', 'cost of is valueable', '2023-02-05 07:26:24', 1),
+(2, 'Yash Bablya', 'yash@gmail.com', 'Nice service', '2023-02-09 12:48:40', 1),
+(3, 'Ajay Dangle', 'ajay@gmail.com', 'Want to know price of birthday party', '2023-03-16 12:51:06', 1),
+(4, 'Ganesh', 'ganesh@gmail.com', 'lovely decoration', '2023-03-19 17:53:03', 1);
 
 -- --------------------------------------------------------
 
@@ -121,23 +119,17 @@ CREATE TABLE `tbleventtype` (
 --
 
 INSERT INTO `tbleventtype` (`ID`, `EventType`, `CreationDate`) VALUES
-(1, 'Anniversary', '2020-01-22 07:01:39'),
-(2, 'Birthday Party', '2020-01-22 07:02:34'),
-(3, 'Charity', '2020-01-22 07:02:43'),
-(4, 'Cocktail', '2020-01-22 07:03:00'),
-(5, 'College', '2020-01-22 07:03:11'),
-(6, 'Community', '2020-01-22 07:03:24'),
-(7, 'Concert', '2020-01-22 07:03:35'),
-(8, 'Engagement', '2020-01-22 07:03:51'),
-(9, 'Get Together', '2020-01-22 07:04:04'),
-(10, 'Government', '2020-01-22 07:04:15'),
-(11, 'Night Club', '2020-01-22 07:04:26'),
-(13, 'Post Wedding', '2020-01-22 07:05:01'),
-(14, 'Pre Engagement', '2020-01-22 07:05:18'),
-(15, 'Religious', '2020-01-22 07:05:27'),
-(16, 'Sangeet', '2020-01-22 07:05:43'),
-(17, 'Social', '2020-01-22 07:05:58'),
-(18, 'Wedding', '2020-01-22 07:06:07');
+(1, 'Anniversary', '2023-01-22 07:01:39'),
+(2, 'Birthday Party', '2023-01-22 07:02:34'),
+(3, 'Charity', '2023-01-22 07:02:43'),
+(4, 'College', '2023-01-22 07:03:11'),
+(5, 'Concert', '2023-01-22 07:03:35'),
+(6, 'Engagement', '2023-01-22 07:03:51'),
+(7, 'Get Together', '2023-01-22 07:04:04'),
+(8, 'Night Club', '2023-01-22 07:04:26'),
+(9, 'Post Wedding', '2023-01-22 07:05:01'),
+(10, 'Pre Engagement', '2023-01-22 07:05:18'),
+(11, 'Wedding', '2023-01-22 07:06:07');
 
 -- --------------------------------------------------------
 
@@ -160,8 +152,8 @@ CREATE TABLE `tblpage` (
 --
 
 INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`) VALUES
-(1, 'aboutus', 'About Us', '<b>Online Banquet Booking System</b><div><b>OBBS is one of the Internet\'s largest and trusted Online Banquet Booking Service. OBBShas done several placements locally &amp; globally for top artists.</b></div><div><b><br></b></div><div><b>&nbsp;Test data for testing.&nbsp;</b></div>', NULL, NULL, '2022-02-19 17:54:36'),
-(2, 'contactus', 'Contact Us', 'D-204, Hole Town South West,Delhi-110096,India', 'info@gmail.com', 1234567899, '2022-02-19 17:54:52');
+-- (1, 'aboutus', 'About Us', '<b>Online Banquet Booking System</b><div><b>OBBS is one of the Internet\'s largest and trusted Online Banquet Booking Service. OBBShas done several placements locally &amp; globally for top artists.</b></div><div><b><br></b></div><div><b>&nbsp;Test data for testing.&nbsp;</b></div>', NULL, NULL, '2022-02-19 17:54:36'),
+(1, 'contactus', 'Contact Us', 'D-204, Hole Town South West,Delhi-110096,India', 'info@gmail.com', 1234567899, '2022-02-19 17:54:52');
 
 -- --------------------------------------------------------
 
@@ -182,9 +174,10 @@ CREATE TABLE `tblservice` (
 --
 
 INSERT INTO `tblservice` (`ID`, `ServiceName`, `SerDes`, `ServicePrice`, `CreationDate`) VALUES
-(1, 'Birthday Part', '(we install the DJ equipment before your ceremony or after your wedding breakfast)', '800', '2023-03-24 07:17:43'),
-(2, 'Private Part', '(we install the DJ equipment 1 hour before your selected event start time)', '700', '2023-03-24 07:18:32'),
-(3, 'Custom Party', 'Our ceremony music service is a popular add on to our wedding DJ stay all day hire.', '650', '2023-03-24 07:19:14');
+(1, 'Birthday Part', '("Join us for a night of fun and celeration as we raise a glass an birthday! With delicious food and great music, its sure to be a remember party".)', '800', '2023-03-24 07:17:43'),
+(2, 'Private Part', '("Celebrate your next private party at Nourish, a cozy and elegant venue with delicious food, friendly service and a stylish dinning room.Contact us for make your party special".)', '700', '2023-03-24 07:18:32'),
+(3, 'Custom Party', '("Make your next celebration unforgettable by hosting it at Nourish! Our delicious food and tailor the event to your specific needs and preference".)', '650', '2023-03-24 07:19:14'),
+(4, 'Party DJ', '("we install the DJ equipment 1 hour before your selected event start time")', '700', '2020-01-24 07:18:32');
 
 -- --------------------------------------------------------
 
@@ -206,11 +199,10 @@ CREATE TABLE `tbluser` (
 --
 
 INSERT INTO `tbluser` (`ID`, `FullName`, `MobileNumber`, `Email`, `Password`, `RegDate`) VALUES
-(1, 'Anvi', 9879878979, 'anvi@gmail.com', '202cb962ac59075b964b07152d234b70', '2022-02-15 09:15:51'),
-(2, 'hkjhkj', 4579878687, 'rewrewre@yutuy', '81dc9bdb52d04dc20036dbd8313ed055', '2022-02-15 09:16:44'),
-(3, 'Reetu Singh', 5465465464, 'reetu@gmail.com', '202cb962ac59075b964b07152d234b70', '2022-02-15 12:07:55'),
-(4, 'John Doe', 1234569879, 'John@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2022-02-19 15:17:04'),
-(5, 'Anuj Singh', 1236985211, 'akj@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2022-02-19 17:50:28');
+(1, 'Ganesh', 9879878979, 'ganesh@gmail.com', 'fa1d87bc7f85769ea9dee2e4957321ae', '2023-03-15 09:15:51'),
+(2, 'Aditya', 4579878687, 'adi@gmail.com', 'c46335eb267e2e1cde5b017acb4cd799', '2023-03-15 09:16:44'),
+(3, 'Yash', 5465465464, 'yash@gmail.com', 'c296539f3286a899d8b3f6632fd62274', '2023-03-15 12:07:55'),
+(4, 'Ajay', 1234569879, 'ajay@gmail.com', '29e457082db729fa1059d4294ede3909', '2023-03-19 15:17:04');
 
 --
 -- Indexes for dumped tables
